e329076697ba12d15f4cd605143a3ddb2bdde18f
l3onardodev <leonardo.contato03@gmail.com>
2021-12-31 14:13:29 -0300

    feat: Tailwind config file created

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kenai',
+    title: 'Kenai',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -51,4 +51,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        white: '#f9f9f9',
+        gray: {
+          500: '#a8a8a8',
+          900: '#181818'
+        },
+        black: '#121214',
+        yellow: '#ffa334'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
